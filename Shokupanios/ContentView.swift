2dcda2fdb53b09f5296e2119bb89a2ce310642c2
Nhat <nhatmobile@gmail.com>
2026-01-03 09:53:12 -0800

    Initial Commit

---
+++ Shokupanios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shokupanios
+//
+//  Created by Nhat Tran on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shokupanios/ShokupaniosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShokupaniosApp.swift
+//  Shokupanios
+//
+//  Created by Nhat Tran on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ShokupaniosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
